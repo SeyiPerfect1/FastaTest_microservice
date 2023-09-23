da990646eccae522feaf7070eb53e0bdd28afb14
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2023-09-23 11:31:29 -0400

    added jwt, logger and otpgenerator utilities

---
+++ src/utility/jwt.utility.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+export const ValidateJwt = async (req) => {
+  const signature = req.get("Authorization");
+
+  if (signature) {
+    const payload = jwt.verify(signature.split(" ")[1], process.env.JWT_SECRET);
+    req.user = payload;
+
+    return true;
+  }
+
+  return false;
+};
+
+const genToken = async (data) => {
+  let token;
+  // generate new jwt token for registeration
+  token = jwt.sign(
+    {
+      ...data,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
+  return token;
+};
+
+export const signToken = async (data) => {
+  const token = await genToken({
+    ...data,
+  });
+
+  return token;
+};
