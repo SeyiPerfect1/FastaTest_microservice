336ed5ae97467428c27ce6ad39f188c4b73e3d4d
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2023-09-24 17:47:41 -0400

    fix: all model and migration issues

--- migrations/20230923093146-create-user.js
+++ migrations/20230923093146-create-user.js
@@ -9,11 +9,11 @@ module.exports = {
         primaryKey: true,
         allowNull: false,
       },
-      firstName: {
+      first_name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      lastName: {
+      last_name: {
         type: Sequelize.STRING,
         allowNull: false,
       },
@@ -41,14 +41,11 @@ module.exports = {
       transaction_pin: {
         type: Sequelize.STRING,
       },
-      donation_count: {
-        type: Sequelize.INTEGER,
-      },
-      created_at: {
+      createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      updated_at: {
+      updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
       },
