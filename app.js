3018166f7b7c1c349c6973fc7960e29e106ad36b
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2023-09-28 18:30:44 -0400

    fix: validation and res error

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.get("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).json({
+  res.status(502).json({
     error: err.message,
   });
 });
